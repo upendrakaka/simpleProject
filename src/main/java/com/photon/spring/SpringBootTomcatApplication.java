3a09a7642c2dfe2e64aef3524916d30d6f2c1cb0
Upendra Kaka <upendra_p@photonint.com>
2018-07-31 15:25:34 +0530

    update src and pom file

---
+++ src/main/java/com/photon/spring/SpringBootTomcatApplication.java
@@ -0,0 +1,9 @@
+package com.photon.spring;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootTomcatApplication extends SpringBootServletInitializer{
+
+}
